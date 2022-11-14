742dadb70dcac98b42f5f0de1446ce96a66c32aa
hacklon <harshalr.kadam@gmail.com>
2022-11-14 14:50:10 +0530

    second commit

---
+++ mavendemos/src/main/java/com/lti/demon/UserApp.java
@@ -0,0 +1,30 @@
+package com.lti.demon;
+
+
+
+
+public class TestProducts {
+	public static void main1(String[] args) {
+		
+	}
+
+
+
+   public static void main(String[] args) {
+        
+        System.out.println("testing product" );
+    }
+}
+
+ppublic class TestProducts {
+
+
+
+   public static void main(String[] args) {
+        
+        System.out.println("testing product" );
+    }
+}
+public class UserApp {
+
+}
